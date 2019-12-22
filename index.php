7a0c0ff1e4615f94ee73178cf8db441ca3caa11d
Damian Zamroczynski <damianzamroczynski@gmail.com>
2019-12-22 17:19:14 +0100

    Add database connection and index file
    File:
    index - at the moment file is empty
    database_connection - file has in it comment about connection
    
    folders:
    css
    js
    img
    js
    instructions

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    require_once 'database_connection.php';
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+    <head>
+        <title>Stacja Paliw 4449</title>
+        <link rel="icon" href="img/icon.png">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="author" content="Damian Zamroczynski" />
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/main.css" />
+        
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--[if lt IE 9]>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"</scripts>
+        <![endif]-->
+        <script src="http://cookiealert.sruu.pl/CookieAlert-latest.min.js"></script>
+        <script>CookieAlert.init();</script>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+            Stacja 4449 Bydgoszcz by Damian Zamroczynski &copy; 2019-2020 Kontakt: damianzamroczynski@gmail.com
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+                crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+                crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
