62fbe6ce93ea481d75c3baa09448ebeaf31058ac
Damian Zamroczynski <damianzamroczynski@gmail.com>
2019-12-25 12:34:11 +0100

    Add login form to index and add logout file

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
     session_start();
     require_once 'database_connection.php';
+    if (isset($_SESSION['logged']))
+    {
+        header('Location: user_profile.php');
+        exit();
+    }
     $today = new DateTime();
     $date_string = $today->format('Y-m-d');
+    //change date
     if(isset($_POST['date_to_search']))
     {
         $date_string = filter_input(INPUT_POST, 'date_to_search');
@@ -13,6 +19,43 @@
     {
         $date_query = 'SELECT expiry_dates.id, expiry_dates.expiry_date, products.name FROM expiry_dates INNER JOIN products ON
         products.id=expiry_dates.id_product WHERE expiry_dates.expiry_date="'.$date_string.'" ORDER BY expiry_dates.id';
+    }
+    //login
+    if ((isset($_POST['login'])) || (isset($_POST['password'])))
+    {
+        $login = filter_input(INPUT_POST, 'login');
+        $password = $_POST['password'];
+        require_once 'database_connection.php';
+        $user_query = $db->prepare('SELECT employees.id, employees.login, employees.pass, employees.email, employees.name, 
+        employees.power, employees.last_login, employees.phone_number 
+        FROM employees 
+        WHERE employees.login=:login');
+        $user_query->bindValue(':login', $login, PDO::PARAM_STR);
+        $user_query->execute();
+        $user = $user_query->fetch();
+        if(password_verify($password, $user['pass']))
+        {
+            if ($user)
+            {
+                $_SESSION['logged'] = true;
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_name'] = $user['name'];
+                $_SESSION['user_pass'] = $user['pass'];
+                $_SESSION['user_power'] = $user['power'];
+                $_SESSION['user_email'] = $user['email'];
+                $_SESSION['user_last_login'] = $user['last_login'];
+                $_SESSION['user_phone_number'] = $user['phone_number'];
+                $today = new DateTime();
+                $today_string = $today->format('Y-m-d H:i:s');
+                $db->query('UPDATE employees SET last_login="'.$today_string.'"');
+                header('Location: user_profile.php');
+                exit();
+            }
+        }
+        else
+        {
+            $_SESSION['error'] = '<div class="error">Nie prawidłowy login lub hasło</div>';
+        }
     }
 ?>
 <!DOCTYPE HTML>
@@ -27,6 +70,7 @@
         <link rel="stylesheet" href="css/fontello.css">
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/button4.css" />
+        <link rel="stylesheet" href="css/input.css" />
         <link rel="stylesheet" href="css/main.css" />
         
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
@@ -44,39 +88,58 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="main-logo">
-                            <h1>Stacja Paliw 4449</h1>
+                            <h1><a href="index.php">Stacja Paliw 4449</a></h1>
                         </div>
                     </div>
                 </div>
             </div>
         </header>
         <main>
-        <div class="container-fluid zero-padding separator-bottom">
+            <div class="container-fluid zero-padding separator-bottom">
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="check-date">
                             <h2>Produkty z datą przydatności do <?= $date_string ?></h2>
-                            <?php
-                                $result = $db->query($date_query);
-                                if ($result->rowCount() > 0)
-                                {
-                                    foreach($result as $row) {
-                                        echo '<div class="product">';
-                                        print_r($row['name']);
-                                        echo "</div>";
+                                <?php
+                                    $result = $db->query($date_query);
+                                    if ($result->rowCount() > 0)
+                                    {
+                                        foreach($result as $row) {
+                                            echo '<div class="product">';
+                                            print_r($row['name']);
+                                            echo "</div>";
+                                        }
                                     }
-                                }
-                                else
-                                {
-                                    echo '<div class="product">';
-                                    echo "Brak produktów, które się terminują!";
-                                    echo '</div>';
-                                }
-                            ?>
-                            <form method="post">
+                                    else
+                                    {
+                                        echo '<div class="product">';
+                                        echo "Brak produktów, które się terminują!";
+                                        echo '</div>';
+                                    }
+                                ?>
+                            <form method="post" class="change-date">
                                 Zmień datę:
-                                <input type="date" name="date_to_search" value="<?= $date_string ?>" />
-                                <input type="submit" class="button4" value="Zmień datę" />
+                                <input type="date" name="date_to_search" class="date-to-change" value="<?= $date_string ?>" />
+                                <input type="submit" class="button4 change-date-button" value="Zmień datę" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 separator-top">
+                        <div class="login-panel">
+                            <form method="post">
+                                <?php
+                                    if(isset($_SESSION['error']))
+                                    {
+                                        echo $_SESSION['error'];
+                                        unset($_SESSION['error']);
+                                    }
+                                ?>
+                                <label><h3>Logowanie</h3></label>
+                                <div><label></label><input type="text" name="login" placeholder="Login" class="input-login" /></div>
+                                <div><label></label><input type="password" name="password" placeholder="Hasło" class="input-login" /></div>
+                                <div><input type="submit" value="Zaloguj się" class="button4" /></div>
                             </form>
                         </div>
                     </div>
